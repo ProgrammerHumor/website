71801e1fdf5ce47753df577c6fd80baa6acc2c74
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-02-03 13:41:04 -0500

    Fixed Reddit link box to be about what else we do

--- site/src/redditLink.jsx
+++ site/src/redditLink.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
+import { text } from "./paraForWhatElseWeDo.json";
 class linkForReddit extends Component {
   state = {};
   render() {
     return (
       <div>
+        <h1 className="ui massive inverted header">
+          Aren't you just a reddit page?
+        </h1>
+        <h4 className="ui medium inverted header"> {text} </h4>
         <a
-          className="ui large blue header"
+          className="ui small blue header"
           onClick={this.handleClick}
           style={{ cursor: "pointer" }}
           target="_blank"
           href="https://www.reddit.com/r/ProgrammerHumor/"
         >
-          Link To Reddit
+          But here is a link to our reddit page.
         </a>
         {/* <img src={redditImage} /> */}
       </div>
