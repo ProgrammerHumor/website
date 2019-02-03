abfda7787a321e6ef2fe3705b2b17c5bc1f173ab
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-02-03 00:40:50 -0500

    I fixed some stuff I fucked up with github

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -9,7 +9,7 @@ ReactDOM.render(<App />, document.getElementById("navBar"));
 // ReactDOM.render(<RedditLink />, document.getElementById("redditLink"));
 // ReactDOM.render(<CurrentEvent />, document.getElementById("currentEvent"));
 // ReactDOM.render(<Footer />, document.getElementById("footer"));
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ site/src/redditLink.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+class linkForReddit extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <a
+          className="ui large blue header"
+          onClick={this.handleClick}
+          style={{ cursor: "pointer" }}
+          target="_blank"
+          href="https://www.reddit.com/r/ProgrammerHumor/"
+        >
+          Link To Reddit
+        </a>
+        {/* <img src={redditImage} /> */}
+      </div>
+    );
+  }
+  handleClick = () => {
+    console.log("Hello World");
+  };
+}
+
+export default linkForReddit;
