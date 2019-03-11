e24e022f91792c4ed1caafd0393953bdbcc6ae8e
404UsernameNotFound404 <37249924+404UsernameNotFound404@users.noreply.github.com>
2019-03-11 18:42:16 -0400

    worked on styling for header

--- site/src/Pages/Hackathon/Sponsors.tsx
+++ site/src/Pages/Hackathon/Sponsors.tsx
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
 import { Header, Grid, Image } from "semantic-ui-react";
 
-export default class Theme extends Component<any>  {
+export default class Theme extends Component<any> {
   render() {
     return (
       <div id="Sponsors" className="themeBox">
-        <Header className="theme">Sponsors:</Header>
+        <Header className="theme">Sponsors</Header>
         <Grid stackable columns="equal" divided>
           <Grid.Row>
             <Grid.Column>
-              <Image src='https://www.corsair.com/corsairmedia/sys_master/productcontent/Corsair_logo_1A_black_800px.png' />
+              <Image
+                centered
+                size="huge"
+                src="https://www.corsair.com/corsairmedia/sys_master/productcontent/Corsair_logo_1A_black_800px.png"
+              />
             </Grid.Column>
             <Grid.Column>
-              <Image centered size="small" src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/DigitalOcean_logo.svg/1200px-DigitalOcean_logo.svg.png' />
+              <Image
+                centered
+                size="small"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/DigitalOcean_logo.svg/1200px-DigitalOcean_logo.svg.png"
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </div>
-    )
+    );
   }
 }
--- site/src/Pages/Hackathon/Theme.tsx
+++ site/src/Pages/Hackathon/Theme.tsx
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
 import { Container, Header } from "semantic-ui-react";
 
-export default class Theme extends Component<any>  {
+export default class Theme extends Component<any> {
   render() {
     return (
       <div id="Theme" className="themeBox">
-        <Header className="theme">Theme:</Header>
+        <Header className="theme">Theme</Header>
         <Header className="overeng">Overengineering</Header>
-        <Header className="themeDesc">Come up with programs that make trivial actions complicated!</Header>
+        <Header className="themeDesc">
+          Come up with programs that make trivial actions complicated!
+        </Header>
       </div>
-    )
+    );
   }
 }
