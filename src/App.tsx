d62c81c9ec3304d52d978943d706ce832dcdf89d
xXAligatorXx <xXAligatorXx@outlook.com>
2019-05-01 17:29:12 -0400

    fixed a few bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import 'semantic-ui-css/semantic.min.css'
 import Navigation from "./NavBar/Navigation";
 
 class App extends Component {
--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("navBar"));
+ReactDOM.render(<App />, document.getElementById("ProgrammerHumor"));
 // ReactDOM.render(<Welcome />, document.getElementById("welcomeCom"));
 // ReactDOM.render(<RedditLink />, document.getElementById("redditLink"));
 // ReactDOM.render(<CurrentEvent />, document.getElementById("currentEvent"));
