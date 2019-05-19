6803592c1b823552bea63d197dc872cfd856f893
Ali Abdoli <xXAligatorXx@outlook.com>
2019-05-19 19:01:29 -0400

    got rid of placeholder text

--- src/Pages/Hackathon/Prizes.tsx
+++ src/Pages/Hackathon/Prizes.tsx
@@ -11,15 +11,15 @@ export default class Prizes extends Component {
           <Grid.Row>
             <Grid.Column>
               <Header size="medium" inverted>1st place</Header>
-              <div className="prizeDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              <div className="prizeDesc"></div>
             </Grid.Column>
             <Grid.Column>
               <Header size="medium" inverted>2nd place</Header>
-              <div className="prizeDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              <div className="prizeDesc"></div>
             </Grid.Column>
             <Grid.Column>
               <Header size="medium" inverted>3rd place</Header>
-              <div className="prizeDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              <div className="prizeDesc"></div>
             </Grid.Column>
           </Grid.Row>
         </Grid>
