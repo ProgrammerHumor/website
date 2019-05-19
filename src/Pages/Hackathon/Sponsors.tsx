f051827c3240adbce5010a9a58d4d20c8da5edce
Ali Abdoli <xXAligatorXx@outlook.com>
2019-05-19 12:16:54 -0400

    Got rid of images for now.

--- src/Pages/Hackathon/Sponsors.tsx
+++ src/Pages/Hackathon/Sponsors.tsx
@@ -9,20 +9,6 @@ export default class Theme extends Component {
         <Header className="theme">Sponsors</Header>
         <Grid verticalAlign="middle" stackable columns="equal" divided>
           <Grid.Row>
-            <Grid.Column>
-              <Image
-                centered
-                size="huge"
-                src="https://www.corsair.com/corsairmedia/sys_master/productcontent/Corsair_logo_1A_black_800px.png"
-              />
-            </Grid.Column>
-            <Grid.Column>
-              <Image
-                centered
-                size="small"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/DigitalOcean_logo.svg/1200px-DigitalOcean_logo.svg.png"
-              />
-            </Grid.Column>
           </Grid.Row>
         </Grid>
         <Button as={Link} smooth to="/Hackathon#Prizes" className="scrollDown" circular icon="chevron down" />
