468220f50466219a595bf17b76309ebef197db8b
jman005 <justinyoung106@gmail.com>
2019-05-30 02:13:57 +0000

    Update FAQ.tsx
    replaced filler text with actual info

--- src/Pages/Hackathon/FAQ.tsx
+++ src/Pages/Hackathon/FAQ.tsx
@@ -11,21 +11,21 @@ export default class FAQ extends Component {
           <Grid.Row>
             <Grid.Column>
               <Header size="medium" inverted>What is a hackathon?</Header>
-              <div className="prizeDesc">It's an event where teams compete by coding!.</div>
+              <div className="prizeDesc">It's an event where teams compete by coding!. You're given a theme to program something based on, and must complete your project within a given timeframe. After the due date, judges will vote for their favorite entry, and prizes will be distributed to the winners. </div>
             </Grid.Column>
             <Grid.Column>
               <Header size="medium" inverted>How can one register?</Header>
               <div className="prizeDesc"><Link to="/register">Click here</Link></div>
             </Grid.Column>
             <Grid.Column>
-              <Header size="medium" inverted>How can one submit project?</Header>
-              <div className="prizeDesc">Ipsum incididunt fugiat quis fugiat id labore eu ipsum. Est non eu laborum ipsum anim incididunt cillum in laborum anim. Dolore reprehenderit proident proident ex amet et ea cillum mollit nulla qui nisi aute laboris. Occaecat id minim in sit exercitation deserunt sit id est cillum adipisicing minim laboris. Dolor tempor incididunt sunt non deserunt labore ad. Incididunt eiusmod laboris occaecat in anim deserunt id id. Veniam enim cupidatat duis nisi.</div>
+              <Header size="medium" inverted>How can one submit their entry?</Header>
+              <div className="prizeDesc">Once you've completed your entry, upload it to either GitHub or GitLab and fill out our form (link TBA). See the "Submissions" page for more details.</div>
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
             <Grid.Column>
-              <Header size="medium" inverted>How will projects be marked?</Header>
-              <div className="prizeDesc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+              <Header size="medium" inverted>How will projects be judged?</Header>
+              <div className="prizeDesc">Shortly after the due date, our judges will assess your entry based on the judging criteria specified above (code quality is not included). </div>
             </Grid.Column>
             <Grid.Column>
               <Header size="medium" inverted>How can I form teams?</Header>
