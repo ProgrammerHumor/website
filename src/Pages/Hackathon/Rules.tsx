755336fa51eabcab02a94a8565851c91193125af
jman005 <justinyoung106@gmail.com>
2019-05-30 01:56:18 +0000

    Update Rules.tsx

--- src/Pages/Hackathon/Rules.tsx
+++ src/Pages/Hackathon/Rules.tsx
@@ -10,12 +10,16 @@ export default class Rules extends Component {
         <Grid stackable columns="equal" divided>
           <Grid.Row>
             <Grid.Column>
-              <Header size="medium" inverted>Don't be an Asshole</Header>
+              <Header size="medium" inverted>Be civilized and respectful</Header>
               <div className="prizeDesc">Please be nice to each other. This is mainly for fun and there is no reason to bring any hate into the Hackathon. As always, remember the human.</div>
             </Grid.Column>
             <Grid.Column>
               <Header size="medium" inverted>Maximum team of 4</Header>
-              <div className="prizeDesc">To keep things somewhat fair, we unfortunately have to have a cap on team size.</div>
+              <div className="prizeDesc">To keep things somewhat fair, we unfortunately have to have a cap on team size. Prizes will be distributed evenly between winning teams.</div>
+            </Grid.Column>
+            <Grid.Column>
+              <Header size="medium" inverted>Code must be legal and harmless</Header>
+              <div className="prizeDesc">Your code must be 100% legal and only use libraries which the creator permits you to use. Any entry found to contain malicious code, including but not limited to: Cryptocurrency miners (unless this is directly related to your submission), viruses/file infectors, adware or spyware will be immediately disqualified, and said entrants may be banned from future ProgrammerHumor hackathons.</div>
             </Grid.Column>
           </Grid.Row>
         </Grid>
