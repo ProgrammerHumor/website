328aa630f33c145ed91b4d5ddf2fcb7fe27be0e3
xXAligatorXx <xXAligatorXx@outlook.com>
2019-07-11 21:02:06 -0400

    added Steve's image

--- src/Pages/Hackathon/AdminsProfileContainer.jsx
+++ src/Pages/Hackathon/AdminsProfileContainer.jsx
@@ -7,6 +7,7 @@ import delite from "../../images/people/delite.jpg";
 import jman from "../../images/people/jman.png";
 import pixxl from "../../images/people/pixxl.png";
 import dejay from "../../images/people/dejay.png"
+import stve from "../../images/people/stve.jpg"
 import img3 from "../../images/AndySamberg.jpg";
 import { Responsive } from "semantic-ui-react";
 
@@ -34,7 +35,7 @@ class AdminsProflies extends Component {
       {
         key: 2,
         title: "Streamer and Judge",
-        img: img3,
+        img: stve,
         name: "Steve",
         twitterLink: "https://twitter.com/countingsort",
         redditLink: "https://www.reddit.com/user/SteveCCL",
