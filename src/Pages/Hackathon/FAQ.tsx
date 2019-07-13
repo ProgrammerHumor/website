281625baf4fb8a282a9444001c28210a304a6562
xXAligatorXx <xXAligatorXx@outlook.com>
2019-07-12 23:16:15 -0400

    fixed FAQ and Rules on mobile

--- src/Pages/Hackathon/FAQ.tsx
+++ src/Pages/Hackathon/FAQ.tsx
@@ -8,27 +8,27 @@ export default class FAQ extends Component {
         <Header className="theme">Frequently Asked Questions</Header>
         <Dropdown className="dropDown" fluid multiple text="What is a hackathon?">
           <DropdownMenu>
-            <Message>It's an event where teams compete by coding! You're given a theme to program something based on, and must complete your project within a given timeframe. After the due date, judges will vote for their favorite entry, and prizes will be distributed to the winners.</Message>            
+            <Message className="dropText">It's an event where teams compete by coding! You're given a theme to program something based on, and must complete your project within a given timeframe. After the due date, judges will vote for their favorite entry, and prizes will be distributed to the winners.</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How can one submit their entry?">
           <DropdownMenu>
-            <Message>Once you've completed your entry, upload it to either GitHub or GitLab and fill out our form (link TBA). See the "Submissions" page for more details.</Message>            
+            <Message className="dropText">Once you've completed your entry, upload it to either GitHub or GitLab and fill out our form (link TBA). See the "Submissions" page for more details.</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How will projects be judged?">
           <DropdownMenu>
-            <Message>Shortly after the due date, our judges will assess your entry based on the judging criteria specified above (code quality is not included).</Message>            
+            <Message className="dropText">Shortly after the due date, our judges will assess your entry based on the judging criteria specified above (code quality is not included).</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How can I form teams?">
           <DropdownMenu>
-            <Message>You can invite your friends, or, if you have never heard of those magical creatures, look for people on the subreddit or our Discord server. You can even go solo! Anything works as long as team.length &lt;= 4</Message>            
+            <Message className="dropText">You can invite your friends, or, if you have never heard of those magical creatures, look for people on the subreddit or our Discord server. You can even go solo! Anything works as long as team.length &lt;= 4</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="Help! I need additional info.">
           <DropdownMenu>
-            <Message>You can ask either on the subreddit with the Hackathon flair, or on the <a href="https://discord.gg/7cbeUvC">Discord Server</a>. We recommend joining Discord since you would be more likely to get assistance quickly there.</Message>            
+            <Message className="dropText"> You can ask either on the subreddit with the Hackathon flair, or on the <a href="https://discord.gg/7cbeUvC">Discord Server</a>. We recommend joining Discord since you would be more likely to get assistance quickly there.</Message>            
           </DropdownMenu>
         </Dropdown>
     </div>
--- src/Pages/Hackathon/Rules.tsx
+++ src/Pages/Hackathon/Rules.tsx
@@ -9,7 +9,7 @@ export default class Rules extends Component {
         <Header className="theme">Rules</Header>
         <Dropdown className="dropDown" fluid multiple text="Be civilized and respectful">
           <DropdownMenu>
-            <Header>Please be nice to each other. This is mainly for fun and there is no reason to bring any hate into the Hackathon. As always, remember the human.</Header>            
+            <Message className="dropText">Please be nice to each other. This is mainly for fun and there is no reason to bring any hate into the Hackathon. As always, remember the human.</Message>            
             <Button style={{
                 marginTop: "2px",
                 }} as="a" href="code-of-conduct.html">Code Of Conduct</Button>
@@ -17,15 +17,15 @@ export default class Rules extends Component {
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="Maximum team of 4">
           <DropdownMenu>
-            <Header>To keep things somewhat fair, we unfortunately have to have a cap on team size. Prizes will be distributed evenly between winning teams.</Header>            
+            <Message className="dropText">To keep things somewhat fair, we unfortunately have to have a cap on team size. Prizes will be distributed evenly between winning teams.</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="Program Rules">
           <DropdownMenu>
-            <Header>Your program must be 100% legal and only use libraries which the creator permits you to use.</Header>
-            <Header>Your may not start programming before the start time.</Header>
-            <Header>The program source code must be available.</Header>           
-            <Header> Any entry found to contain malicious code, including but not limited to: Cryptocurrency miners (unless this is directly related to your submission), viruses/file infectors, adware or spyware will be met with harsh punishment.</Header>
+            <Message className="dropText">Your program must be 100% legal and only use libraries which the creator permits you to use.</Message>
+            <Message className="dropText">Your may not start programming before the start time.</Message>
+            <Message className="dropText">The program source code must be available.</Message>           
+            <Message className="dropText"> Any entry found to contain malicious code, including but not limited to: Cryptocurrency miners (unless this is directly related to your submission), viruses/file infectors, adware or spyware will be met with harsh punishment.</Message>
           </DropdownMenu>
         </Dropdown>
       </div>
