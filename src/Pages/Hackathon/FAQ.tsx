8152e653c0568b8b75834f302d5415982924f8df
xXAligatorXx <xXAligatorXx@outlook.com>
2019-07-14 11:11:40 -0400

    fixed FAQ

--- src/Pages/Hackathon/FAQ.tsx
+++ src/Pages/Hackathon/FAQ.tsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Header, Grid, Button, Dropdown, DropdownMenu, Message } from "semantic-ui-react";
-import { HashLink as Link } from 'react-router-hash-link';
 
 export default class FAQ extends Component {
   render = () => (
@@ -13,22 +12,22 @@ export default class FAQ extends Component {
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How can one submit their entry?">
           <DropdownMenu>
-            <Message className="dropText">Once you've completed your entry, upload it to either GitHub or GitLab and fill out our form (link TBA). See the "Submissions" page for more details.</Message>            
+            <Message className="dropText">Once you've completed your entry, you will fill out our form (link will be provided after the Hackatho starts).</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How will projects be judged?">
           <DropdownMenu>
-            <Message className="dropText">Shortly after the due date, our judges will assess your entry based on the judging criteria specified above (code quality is not included).</Message>            
+            <Message className="dropText">Shortly after the due date, our judges will assess your entry based on the judging criteria(TBA) (code quality is not included).</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="How can I form teams?">
           <DropdownMenu>
-            <Message className="dropText">You can invite your friends, or, if you have never heard of those magical creatures, look for people on the subreddit or our Discord server. You can even go solo! Anything works as long as team.length &lt;= 4</Message>            
+            <Message className="dropText">You can invite your friends, or, if you have never heard of those magical creatures, look for people on the subreddit or our <a href="https://discord.gg/7cbeUvC">Discord Server</a>. You can even go solo! Anything works as long as team.length &lt;= 4</Message>            
           </DropdownMenu>
         </Dropdown>
         <Dropdown className="dropDown" fluid multiple text="Help! I need additional info.">
           <DropdownMenu>
-            <Message className="dropText"> You can ask either on the subreddit with the Hackathon flair, or on the <a href="https://discord.gg/7cbeUvC">Discord Server</a>. We recommend joining Discord since you would be more likely to get assistance quickly there.</Message>            
+            <Message className="dropText"> You can ask either on the subreddit post, or on the <a href="https://discord.gg/7cbeUvC">Discord Server</a>. We recommend joining Discord since you would be more likely to get assistance quickly there.</Message>            
           </DropdownMenu>
         </Dropdown>
     </div>
