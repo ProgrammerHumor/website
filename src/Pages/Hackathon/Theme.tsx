7cf0488c7a69b4548ce9fb149dd6e27327621885
xXAligatorXx <xXAligatorXx@outlook.com>
2019-07-13 21:28:46 -0400

    fixed prize box

--- src/Pages/Hackathon/Theme.tsx
+++ src/Pages/Hackathon/Theme.tsx
@@ -12,7 +12,7 @@ export default class Theme extends Component {
         <Header className="themeDesc">
           Come up with programs that make trivial actions complicated!
         </Header>
-        <div className="prizeDesc">
+        <div className="themeDesc2">
           Our subreddit has had a far-reaching history of making simple applications impossible to use. In our first Hackathon, you'll join that legacy by creating a program in any language of your choice which is as tedious to use as possible. Command line applications, GUIs, websites or anything else under the sun is permitted. We want creativity.
           If you'd like some examples of what to make, go to our subreddit and see posts labeled "Bad UI". 
         </div>
