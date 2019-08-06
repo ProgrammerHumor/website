db6dcc53ab4391d9287a637b918774aacc162ae8
xXAligatorXx <xXAligatorXx@outlook.com>
2019-08-05 20:55:37 -0400

    added reddit to sponsors

--- src/Pages/Hackathon/Sponsors.tsx
+++ src/Pages/Hackathon/Sponsors.tsx
@@ -2,23 +2,26 @@ import React, { Component } from "react";
 import { Button, Header, Grid, Image, Responsive } from "semantic-ui-react";
 import jetbrains from "../../images/Sponsors/jetbrains-grayscale.png";
 import digitalOceans from "../../images/Sponsors/DO_Logo_icon_blue.svg";
+import reddit from "../../images/Sponsors/reddit.svg";
 
 export default class Theme extends Component {
   render = () => (
     <div id="Sponsors">
       <div>
         <Responsive {...Responsive.onlyComputer}>
-        <Grid verticalAlign="middle" stackable columns="10">
+        <Grid verticalAlign="middle" stackable columns="9">
           <Grid.Row>
-          <Grid.Column/>
-          <Grid.Column/>
-          <Grid.Column/>
-          <Grid.Column/>
+            <Grid.Column/>
+            <Grid.Column/>
+            <Grid.Column/>
             <Grid.Column>
               <Image href="https://www.jetbrains.com/" src={jetbrains} size="small" centered/>
             </Grid.Column>
             <Grid.Column>
               <Image href="https://www.digitalocean.com/" src={digitalOceans} size="small" centered/>
+            </Grid.Column>
+            <Grid.Column>
+              <Image href="https://www.reddit.com/" src={reddit} size="small" centered/>
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -28,6 +31,7 @@ export default class Theme extends Component {
           <Grid.Row>
               <Image href="https://www.jetbrains.com/" src={jetbrains} size="tiny" centered/>
               <Image href="https://www.digitalocean.com/" src={digitalOceans} size="tiny" centered/>
+              <Image href="https://www.reddit.com/" src={reddit} size="tiny" centered/>
           </Grid.Row>
         </Grid>
         </Responsive>
